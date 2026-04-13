58f755f56e5950e875f2704e7b3c5914b8758866
pantao <980141374@qq.com>
2026-04-13 11:21:30 +0800

    init

---
+++ plugins/hooost/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const route = ref('')
+const enterAction = ref<any>({})
+
+onMounted(() => {
+  window.ztools.onPluginEnter((action) => {
+    route.value = action.code
+    enterAction.value = action
+  })
+  window.ztools.onPluginOut(() => {
+    route.value = ''
+  })
+})
+</script>
+
+<template>
+  <Hello v-if="route === 'hello'" :enter-action="enterAction" />
+  <Read v-if="route === 'read'" :enter-action="enterAction" />
+  <Write v-if="route === 'write'" :enter-action="enterAction" />
+</template>
